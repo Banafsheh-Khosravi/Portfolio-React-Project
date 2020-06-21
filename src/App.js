e0e0780279140918e37f4f4d7edd4b7a0da0b3a3
Bani Khosravi Nia <bani.khosravinia@perficient.com>
2020-06-21 00:54:13 -0400

    added toggle

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+    //to invoke the constructor of the parent component class
+    super()
+    this.state = { displayBio: false };
+    this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+  }
 
+
+  toggleDisplayBio() {
+    this.setState({ displayBio: !this.state.displayBio });
+  }
+
+
+  render() {
+
+    return (
+      <div>
+        <h1>Hello!</h1>
+        <p>My name is Bani. I am a software developer.</p>
+        <p>I am always looking forward to working on chalenging projects.</p>
+
+        {
+          this.state.displayBio ? (
+            <div>
+              <p>I live in Boston, and I code every day.</p>
+              <p>My favorite language is JavaScript.</p>
+              <p>Beside coding, I also love drawing!</p>
+              <button onClick={this.toggleDisplayBio}>Show Less</button>
+            </div>
+          ) : (
+              <div>
+                <button onClick={this.toggleDisplayBio}>Read More</button>
+              </div>
+            )
+        }
+      </div>
+    )
+  }
+}
 export default App;
